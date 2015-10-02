e0b7713535ef08bf216533b4b8ca655cb1e500f9
De Wet Blomerus <dewet@blomerus.org>
2015-10-02 22:23:17 +0200

    shift rotate

--- quiz.rb
+++ quiz.rb
@@ -1,22 +1,22 @@
-def ask(question, correct_answer)
-  puts question
-  answer = gets.chomp.downcase
-  loop do
-    if answer == correct_answer.downcase
-      puts "you got it right"
-      break
-    else
-      puts "that was wrong"
-      answer = gets.chomp.downcase
-    end
-  end
-end
+@q_and_a = []
 
 def load_questions(from_file)
   File.readlines(from_file).each do |line|
-    a, q = line.split(',')
-    ask(q, a)
+    @q_and_a << line.split(',')
   end
 end
 
 load_questions(ARGV.shift || 'questions.csv')
+
+until @q_and_a.length == 0
+  puts @q_and_a[0][1]
+  answer = gets.chomp.downcase
+  if answer == @q_and_a[0][0].downcase
+    puts "you got it right"
+    @q_and_a.shift
+  else
+    puts "that was wrong"
+    puts "The right answer was #{@q_and_a[0][0]}"
+    @q_and_a = @q_and_a.rotate(1)
+  end
+end
