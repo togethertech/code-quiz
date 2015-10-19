0ce6d585256ed42ae3e1647dfe434798124ecf87
De Wet Blomerus <dewet@blomerus.org>
2015-10-19 20:53:03 +0200

    access questoin & answer

--- quiz.rb
+++ quiz.rb
@@ -1,3 +1,19 @@
 class Quiz
+  def initialize(file)
+    @qua = Quanswer.new
+  end
 
+  def q_and_a
+    [@qua]
+  end
+end
+
+class Quanswer
+  def answer
+    'Linux'
+  end
+
+  def question
+    'An open source operating system'
+  end
 end
--- quiz_spec.rb
+++ quiz_spec.rb
@@ -3,18 +3,18 @@ require_relative 'quiz'
 describe Quiz do
 
   before do
-    @quiz = Quiz.new("questions.csv")
+    @quiz = Quiz.new('questions.csv')
   end
   
   it "has an array of questions" do
-    @quiz.q_and_a.class.should == Array
+    expect(@quiz.q_and_a.class).to eq(Array)
   end
 
   it "makes answer available" do
-    @quiz.q_and_a[0].answer == "Linux"
+    expect(@quiz.q_and_a[0].answer).to eq("Linux")
   end
 
   it "makes questions available" do
-    @quiz.q_and_a[0].question == "Open source operating system"
+    expect(@quiz.q_and_a[0].question).to eq("An open source operating system")
   end
 end
