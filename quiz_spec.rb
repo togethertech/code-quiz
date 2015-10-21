d2fde0f56cd86faebcbced3590b1b5b77f29d1cd
De Wet Blomerus <dewet@blomerus.org>
2015-10-21 10:40:15 +0200

    test load questions

--- quiz_spec.rb
+++ quiz_spec.rb
@@ -15,6 +15,10 @@ describe Quiz do
   end
 
   it "makes questions available" do
-    expect(@quiz.q_and_a[0].question).to eq("An open source operating system")
+    expect(@quiz.q_and_a[0].question).to eq("Most popular open source operating system")
+  end
+
+  it 'loads answers from file' do
+    expect(@quiz.q_and_a[1].answer).to eq("Ruby")
   end
 end
