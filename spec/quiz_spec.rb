dd8f0283b20e376e647747026272a34579f05611
De Wet Blomerus <dewet@blomerus.org>
2015-11-16 12:03:50 +0200

    test current question not finished

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -21,4 +21,10 @@ describe Quiz do
     expect(@quiz.qua[0].class).to eq(Quanswer)
     expect(@quiz.qua[1].class).to eq(Quanswer)
   end
+
+  it 'has a current question' do
+     expect(@quiz.current_question).to include('Most popular open source')
+     @quiz.current_question.shift
+     expect(@quiz.current_question).to include('High level programming lan')     
+  end
 end
